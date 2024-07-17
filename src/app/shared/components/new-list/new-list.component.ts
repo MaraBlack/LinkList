b9b06dafbbdaf8c3e8939d21236898c3faad6b77
Mara Black <me.mara.black@outlook.com>
2024-07-17 19:15:37 +0300

    [dev] new list component wip, name, color and categores are displayed

--- src/app/shared/components/new-list/new-list.component.ts
+++ src/app/shared/components/new-list/new-list.component.ts
@@ -1,17 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ListData } from '../../../models/list-data.interface';
+import { CommonModule } from '@angular/common';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { GroceryCategoryEnum } from '../../../models/grocery-categories.enum';
 
 @Component({
   selector: 'app-new-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, FloatLabelModule, InputTextModule],
   templateUrl: './new-list.component.html',
   styleUrl: './new-list.component.scss',
 })
-export class NewListComponent {
+export class NewListComponent implements OnInit{
+  newListData: ListData = {
+    id: '',
+    name: '',
+    items: [],
+    color: '',
+    dateCreated: '',
+    lastDateAccessed: '',
+    isFav: false,
+  };
+
+  colors = [
+    'gray-500',
+    'green-500',
+    'teal-500',
+    'blue-500',
+    'purple-500',
+    'red-500',
+    'orange-500',
+    'yellow-500',
+  ];
+
+  categores!: string[];
+
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.categores = Object.values(GroceryCategoryEnum);
+  }
 
   onBackClick() {
     this.router.navigate(['myLists']);
+  }
+
+  setColor(color: string){
+    this.newListData.color = color;
   }
 }
